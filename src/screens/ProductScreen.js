687e9de4dcdc18bc1be70e3cf9c99fa887ecd213
Vicky <kumar.v005@gmail.com>
2021-03-20 14:06:57 +0530

    added redux to fetch product list and sinle product

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { detailsProduct } from '../actions/productActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import Rating from '../components/Rating';
-import data from '../data'
 
 function ProductScreen(props) {
-    const product = data.products.find(x => x._id === props.match.params.id);
-    if(!product) {
-        return <div>Product Not Found!</div>
-    }
+    const dispatch = useDispatch();
+    const productId = props.match.params.id;
+    const productDetails = useSelector( (state) => state.productDetails);
+    const { loading, error, product } = productDetails;
+    useEffect(() => {
+        dispatch(detailsProduct(productId));
+    }, []);
     return (
         <div>
+            { loading ? (
+                <LoadingBox></LoadingBox>
+            ) : error ? (
+                <MessageBox variant="danger">{error}</MessageBox>
+            ) : (
+                <div>
             <Link to="/">Back to result</Link>
             <div className="row top">
                 <div className="col-2">
@@ -46,7 +58,7 @@ function ProductScreen(props) {
                                         {product.countInStock > 0 ? (
                                             <span className="success">In Stock</span>
                                         ) : (
-                                            <span className="error">Unavailable</span>
+                                            <span className="danger">Unavailable</span>
                                         )}
                                     </div>
                                 </div>
@@ -59,6 +71,9 @@ function ProductScreen(props) {
                 </div>
             </div>
         </div>
+            )}
+        </div>
+        
     )
 }
 
