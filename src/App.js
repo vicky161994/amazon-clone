691451eb22f733dba7044e74dfaf3e2a5fe7db1f
Vicky <kumar.v005@gmail.com>
2021-04-24 13:23:23 +0530

    added payment method screen

--- src/App.js
+++ src/App.js
@@ -1,50 +1,54 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
-import { signout } from './actions/userActions';
-import CartScreen from './screens/CartScreen';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import { Shippingaddressscreen } from './screens/ShippingAddressScreen';
-import SigninScreen from './screens/SigninScreen';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import { signout } from "./actions/userActions";
+import CartScreen from "./screens/CartScreen";
+import HomeScreen from "./screens/HomeScreen";
+import PaymentMethodScreen from "./screens/PaymentMethodScreen";
+import ProductScreen from "./screens/ProductScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import { Shippingaddressscreen } from "./screens/ShippingAddressScreen";
+import SigninScreen from "./screens/SigninScreen";
 
 function App() {
-  const cart = useSelector(state => state.cart);
+  const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-  const userSignin = useSelector(state => state.userSignin);
+  const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   const dispatch = useDispatch();
   const singoutHandler = async () => {
     dispatch(signout());
-  }
+  };
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <Link className="brand" to="/">amazona</Link>
+            <Link className="brand" to="/">
+              amazona
+            </Link>
           </div>
           <div>
-            <Link to="/cart">Cart
-            {cartItems.length > 0 && (
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
                 <span className="badge">{cartItems.length}</span>
               )}
             </Link>
-            {
-              userInfo ? (
-                <div className="dropdown">
-                  <Link to="#">{userInfo.name} <i className="fa fa-caret-down"></i></Link>
-                  <ul className="dropdown-content">
-                    <Link to="signout" onClick={singoutHandler}>Sign Out</Link>
-                  </ul>
-                </div>
-              ) :
-                (
-                  <Link to="/signin">Sign In</Link>
-                )
-            }
-
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <Link to="signout" onClick={singoutHandler}>
+                    Sign Out
+                  </Link>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
           </div>
         </header>
         <main>
