c9c832780f579fcfbe9975a1b059a4dd2ed053e2
Vicky <kumar.v005@gmail.com>
2021-04-24 13:12:57 +0530

    payment method screen implemented

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,61 +1,72 @@
 import axios from "axios";
-import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL } from "../constants/userConstants"
+import {
+  USER_SIGNIN_FAIL,
+  USER_SIGNIN_REQUEST,
+  USER_SIGNIN_SUCCESS,
+  USER_SIGNOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+} from "../constants/userConstants";
 
 export const signin = (email, password) => async (dispatch) => {
+  dispatch({
+    type: USER_SIGNIN_REQUEST,
+    payload: { email, password },
+  });
+  try {
+    const { data } = await axios.post("/api/users/signin", { email, password });
     dispatch({
-        type: USER_SIGNIN_REQUEST,
-        payload: { email, password }
+      type: USER_SIGNIN_SUCCESS,
+      payload: data,
     });
-    try {
-        const { data } = await axios.post('/api/users/signin',
-            { email, password });
-        dispatch({
-            type: USER_SIGNIN_SUCCESS,
-            payload: data
-        });
-        localStorage.setItem('userInfo', JSON.stringify(data));
-    } catch (err) {
-        console.log(err.message);
-        dispatch({
-            type: USER_SIGNIN_FAIL,
-            payload:
-                err.respose && err.respose.data.message
-                    ? err.respose.data.message
-                    : err.message,
-        })
-    }
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (err) {
+    console.log(err.message);
+    dispatch({
+      type: USER_SIGNIN_FAIL,
+      payload:
+        err.respose && err.respose.data.message
+          ? err.respose.data.message
+          : err.message,
+    });
+  }
 };
 
 export const register = (name, email, password) => async (dispatch) => {
-    dispatch({
-        type: USER_REGISTER_REQUEST,
-        payload: { email, password }
+  dispatch({
+    type: USER_REGISTER_REQUEST,
+    payload: { email, password },
+  });
+  try {
+    const { data } = await axios.post("/api/users/register", {
+      name,
+      email,
+      password,
     });
-    try {
-        const { data } = await axios.post('/api/users/register',
-            { name, email, password });
-        dispatch({
-            type: USER_REGISTER_SUCCESS,
-            payload: data
-        });
-        dispatch({
-            type: USER_SIGNIN_SUCCESS,
-            payload: data
-        });
-        localStorage.setItem('userInfo', JSON.stringify(data));
-    } catch (error) {
-        dispatch({
-            type: USER_REGISTER_FAIL,
-            payload:
-                error.respose && error.respose.data.message
-                    ? error.respose.data.message
-                    : error.message,
-        })
-    }
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
+    dispatch({
+      type: USER_SIGNIN_SUCCESS,
+      payload: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.respose && error.respose.data.message
+          ? error.respose.data.message
+          : error.message,
+    });
+  }
 };
 
 export const signout = () => (dispatch) => {
-    localStorage.removeItem('userInfo');
-    localStorage.removeItem('cartItems');
-    dispatch({ type: USER_SIGNOUT });
-}
+  localStorage.removeItem("userInfo");
+  localStorage.removeItem("cartItems");
+  localStorage.removeItem("shippingAddress");
+  dispatch({ type: USER_SIGNOUT });
+};
