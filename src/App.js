c8f3606655266e0b2329351172a978bc244d7201
Vicky <kumar.v005@gmail.com>
2021-04-26 09:20:37 +0530

    place order implementation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { signout } from "./actions/userActions";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import ProductScreen from "./screens/ProductScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import { Shippingaddressscreen } from "./screens/ShippingAddressScreen";
@@ -58,6 +59,7 @@ function App() {
           <Route path="/register" component={RegisterScreen} />
           <Route path="/shipping" component={Shippingaddressscreen} />
           <Route path="/payment" component={PaymentMethodScreen} />
+          <Route path="/placeorder" component={PlaceOrderScreen} />
 
           <Route path="/" component={HomeScreen} exact />
         </main>
