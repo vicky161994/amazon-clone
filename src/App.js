ea949e1569ad9616aee97149dc29e7b3ff7ecf23
Vicky <kumar.v005@gmail.com>
2021-05-06 19:50:55 +0530

    order page implemented, internet connection, battery status functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Link, Route } from "react-router-dom";
 import { signout } from "./actions/userActions";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
+import OrderScreen from "./screens/OrderScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import ProductScreen from "./screens/ProductScreen";
@@ -12,6 +13,10 @@ import { Shippingaddressscreen } from "./screens/ShippingAddressScreen";
 import SigninScreen from "./screens/SigninScreen";
 
 function App() {
+  const [isOnline, setIsOnline] = useState(true);
+  const [batteryStatus, setBatteryStatus] = useState(100);
+  const [batteryMessage, setBatteryMessage] = useState("");
+  const [batteryChargingMessage, setBatteryChargingMessage] = useState("");
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
   const userSignin = useSelector((state) => state.userSignin);
@@ -20,13 +25,81 @@ function App() {
   const singoutHandler = async () => {
     dispatch(signout());
   };
+
+  window.addEventListener("online", () => setIsOnline(true));
+  window.addEventListener("offline", () => setIsOnline(false));
+  window.addEventListener("levelchange", () =>
+    navigator.getBattery().then((battery) => console.log(battery.level))
+  );
+  var batteryPromise;
+  if ("getBattery" in navigator) {
+    batteryPromise = navigator.getBattery();
+  } else {
+    batteryPromise = Promise.resolve(navigator.battery);
+  }
+
+  batteryPromise.then(function (battery) {
+    battery.addEventListener("levelchange", () => {
+      if (battery.level < 21 && battery.charging === false) {
+        setBatteryStatus(battery.level * 100);
+        setBatteryMessage(
+          `${battery.level * 100}% battery left, Please plug the charger`
+        );
+      }
+    });
+    battery.addEventListener("chargingchange", () => {
+      if (battery.charging) {
+        setBatteryChargingMessage("Charging");
+        setBatteryStatus(100);
+      }
+      if (battery.charging === false) {
+        setBatteryChargingMessage("");
+        setBatteryStatus(battery.level * 100);
+      }
+    });
+  });
+
+  if (isOnline) {
+    setTimeout(() => {
+      setIsOnline(undefined);
+    }, 5000);
+  }
+
+  if (batteryChargingMessage !== "") {
+    setTimeout(() => {
+      setBatteryChargingMessage("");
+      setBatteryStatus(100);
+    }, 5000);
+  }
   return (
     <BrowserRouter>
+      {isOnline === false && (
+        <div id="onlinediv1">
+          <span id="onlinespan1">No internet connection</span>
+        </div>
+      )}
+      {isOnline && (
+        <div id="offlinediv1">
+          <span id="offlinespan1">Back online</span>
+        </div>
+      )}
+
+      {batteryStatus < 21 && (
+        <div id="onlinediv1">
+          <span id="offlinespan1">{batteryMessage}</span>
+        </div>
+      )}
+
+      {batteryChargingMessage !== "" && (
+        <div id="offlinediv1">
+          <span id="offlinespan1">{batteryChargingMessage}</span>
+        </div>
+      )}
       <div className="grid-container">
         <header className="row">
           <div>
             <Link className="brand" to="/">
-              amazona
+              amazon
             </Link>
           </div>
           <div>
@@ -60,6 +133,7 @@ function App() {
           <Route path="/shipping" component={Shippingaddressscreen} />
           <Route path="/payment" component={PaymentMethodScreen} />
           <Route path="/placeorder" component={PlaceOrderScreen} />
+          <Route path="/order/:id" component={OrderScreen} />
 
           <Route path="/" component={HomeScreen} exact />
         </main>
